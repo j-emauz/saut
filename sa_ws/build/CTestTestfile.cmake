ef96f8219afdda9d83549c0aeda9ac7396380375
j-emauz <joaoemauz2000@gmail.com>
2022-05-16 15:15:40 -0700

    gazebo cenas

---
+++ sa_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/kunfuxu/saut/sa_ws/src
+# Build directory: /home/kunfuxu/saut/sa_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ua_ros_p3dx/p3dx_control")
+subdirs("ua_ros_p3dx/p3dx_description")
+subdirs("ua_ros_p3dx/p3dx_gazebo")
